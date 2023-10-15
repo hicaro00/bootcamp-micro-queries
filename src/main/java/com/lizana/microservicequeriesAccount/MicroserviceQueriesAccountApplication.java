bfc8249506b293a4db430e5b18698d5ada42f751
humberto <humberto.lizana@gmail.com>
2023-10-14 23:51:57 -0500

    first commit

---
+++ src/main/java/com/lizana/microservicequeriesAccount/MicroserviceQueriesAccountApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.microservicequeriesAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceQueriesAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceQueriesAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizana/microservicequeriesAccount/MicroserviceQueriesAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizana.microservicequeriesAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceQueriesAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
